fed2cfd246ada07b82d477573d1e32f6111f3446
rohit <campon99@gmail.com>
2017-03-20 21:43:18 -1200

    Added the file structure
    As from scoth.io firts auth. arcticle

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var passport = require('passport');
 var app = express();
-app.post('/authenticate', passport.authenticate('local'), function(req, res) {
+var LocalStrategy = require('passport-local').Strategy;
+app.post('/login', passport.authenticate('local'), function(req, res) {
 
     console.log("Logged in!!");
 
 });
+app.get('/', function(req, res) {
+    res.render('index');
+})
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
